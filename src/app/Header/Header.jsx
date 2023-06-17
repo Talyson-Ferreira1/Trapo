cba4188ec3a21733151e628816306405018e1b65
Talyson <talyson.campos13@gmail.com>
2023-06-17 15:49:41 -0300

    first commit

---
+++ src/app/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './style.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+
+export default function Header() {
+  return (
+    <header>
+      <ul>
+        <li>Feminino</li>
+        <li>Masculino</li>
+        <li>Camisas</li>
+        <li>Calças</li>
+        <li>Moletom</li>
+        <li>Sapatos</li>
+      </ul>
+      <button className="finalize_purchase">Finalizar compra</button>
+      <button className="icon-car-shopping">
+        <FontAwesomeIcon className="icone" icon={faCartShopping} />
+      </button>
+    </header>
+  );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+import Header from './Header/Header';
+import styles from './page.module.css';
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.header_global}>
+        <img src="img/Logo.png" />
+        <button>Entrar</button>
+      </div>
+      <Header />
+    </main>
+  );
+}
