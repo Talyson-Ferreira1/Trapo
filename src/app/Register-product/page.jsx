a0bf523a70620d8dea2218c834b210267a577bfc
Talyson <talyson.campos13@gmail.com>
2023-06-23 14:03:55 -0300

    feature: conclui o algoriimo que envia para o banco de dados : resolver o problema do toast que esta quebrando o layout

--- src/app/Register-product/page.jsx
+++ src/app/Register-product/page.jsx
@@ -1,6 +1,13 @@
 'use client';
 import { useEffect, useState } from 'react';
-import { getFirestore, collection, setDoc, doc, getDoc } from 'firebase/firestore';
+import {
+  getFirestore,
+  collection,
+  setDoc,
+  doc,
+  getDoc,
+} from 'firebase/firestore';
+import { getStorage, ref, uploadBytes } from 'firebase/storage';
 import { appFirebase } from '@/services/firebase';
 import { ToastContainer, toast } from 'react-toastify';
 
@@ -11,48 +18,34 @@ import 'react-toastify/dist/ReactToastify.css';
 import './style.css';
 
 export default function Register() {
-  const [toastMessage, setToastMessage] = useState(false)
-  const [submitImage, setSubmitImage] = useState(false)
+  const [toastMessage, setToastMessage] = useState(false);
+  const [submitImage, setSubmitImage] = useState(false);
   const [currentProduct, setCurrentProduct] = useState({
-    productInformation:"",
-    productImage:""
-  })
-  
+    productInformation: '',
+    productImages: '',
+  });
 
   const db = getFirestore(appFirebase);
+  const storage = getStorage(appFirebase);
 
   const SendProductCollection = collection(db, 'Product');
   const docRef = doc(SendProductCollection, 'AllProducts');
 
-  const sendAllProductInformation = () => {
-    const id = generateProductId();
-    sendInformation(id, currentProduct.productInformation)
-
-  };
-
-  const getProductInformation = (info) => {  
-    setCurrentProduct(prev => ({
+  const getProductInformation = (info) => {
+    setCurrentProduct((prev) => ({
       ...prev,
-      productInformation: info
-    }))
+      productInformation: info,
+    }));
 
-    setSubmitImage(true)
+    setSubmitImage(true);
   };
 
-  useEffect(()=>{
-
-    if(currentProduct.productInformation !== ""){
-      console.log(currentProduct.productInformation)
-
-    }
-    
-    if(currentProduct.productImage !== ""){
-      console.log(currentProduct.productImage)
-      
-    }
-
-
-  },[currentProduct])
+  const getProductImage = (images) => {
+    setCurrentProduct((prev) => ({
+      ...prev,
+      productImages: images,
+    }));
+  };
 
   const generateProductId = () => {
     const characters =
@@ -69,74 +62,118 @@ export default function Register() {
   };
 
   const sendInformation = async (id, produto) => {
-
-    setToastMessage(true)
+    setToastMessage(true);
 
     try {
       const docSnapshot = await getDoc(docRef);
       const existingData = docSnapshot.exists() ? docSnapshot.data() : {};
-  
+
       const newData = {
         ...existingData,
         [id]: {
           ...produto,
         },
       };
-  
+
       await setDoc(docRef, newData);
-  
+
       console.log('Dados adicionados com sucesso!');
       toast.success('Produto cadastrado');
-
     } catch (error) {
       console.error('Erro ao adicionar dados:', error);
       toast.error('Erro ao cadastrar');
+    }
+  };
 
+  const sendImages = async (id, images) => {
+    try {
+      const storageRef = ref(storage, `products/${id}`);
+
+      let image1 = images.image_1;
+      let image2 = images.image_2;
+      let image3 = images.image_3;
+      let image4 = images.image_4;
+
+      if (image1 !== '') {
+        const imageRef = ref(storageRef, `image_1`);
+        uploadBytes(imageRef, images.image_1);
+      }
+
+      if (image2 !== '') {
+        const imageRef = ref(storageRef, `image_2`);
+        uploadBytes(imageRef, images.image_2);
+      }
+
+      if (image3 !== '') {
+        const imageRef = ref(storageRef, `image_3`);
+        uploadBytes(imageRef, images.image_3);
+      }
+
+      if (image4 !== '') {
+        const imageRef = ref(storageRef, `image_4`);
+        uploadBytes(imageRef, images.image_4);
+      }
+
+      await Promise.all(uploadPromises);
+
+      console.log('Todas as imagens foram enviadas com sucesso!');
+    } catch (error) {
+      console.error('Erro ao enviar as imagens:', error);
     }
   };
 
   toast.success({
-    position: "top-right",
+    position: 'top-right',
     autoClose: 2000,
     hideProgressBar: false,
     closeOnClick: true,
     pauseOnHover: true,
     draggable: true,
     progress: undefined,
-    theme: "colored",
+    theme: 'colored',
   });
 
-  toast.error( {
-    position: "top-right",
+  toast.error({
+    position: 'top-right',
     autoClose: 2000,
     hideProgressBar: false,
     closeOnClick: true,
     pauseOnHover: true,
     draggable: true,
     progress: undefined,
-    theme: "colored",
+    theme: 'colored',
   });
 
-    
+  useEffect(() => {
+    if (
+      currentProduct.productInformation !== '' &&
+      currentProduct.productImages !== ''
+    ) {
+      const id = generateProductId();
+
+      sendInformation(id, currentProduct.productInformation);
+      sendImages(id, currentProduct.productImages);
+    }
+  }, [currentProduct]);
+
   return (
     <main className="container-Register">
-
-      {(toastMessage)&&(
-
+      {toastMessage && (
         <ToastContainer
-        position="top-right"
-        autoClose={2000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
+          className="toast-position"
+          position="top-right"
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
         />
       )}
-      <RegisterProductImage submit={submitImage} />
+      <RegisterProductImage submit={submitImage} sendImage={getProductImage} />
       <RegisterProductInfo sendInfo={getProductInformation} />
     </main>
   );
--- src/app/components/register-product-image/Upload-Image.jsx
+++ src/app/components/register-product-image/Upload-Image.jsx
@@ -1,87 +1,64 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faImages, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { useFormik } from 'formik';
 import PropTypes from 'prop-types';
 
 import './style.css';
-export default function UploadImage({ submit }) {
+
+export default function UploadImage({ submit, sendImage }) {
   const [images, setImages] = useState({});
-
-  const handleImageChange = (event) => {
-    const fileInput = event.target;
-    const file = fileInput.files[0];
-
-    if (file) {
-      const reader = new FileReader();
-
-      reader.onload = (e) => {
-        const imageData = e.target.result;
-        setImages((prevImages) => ({
-          ...prevImages,
-          [fileInput.name]: imageData,
-        }));
-      };
-
-      reader.readAsDataURL(file);
-    }
-  };
-
-  const handlePrimaryImageChange = (event) => {
-    const fileInput = event.target;
-    const file = fileInput.files[0];
-
-    if (file) {
-      const reader = new FileReader();
-
-      reader.onload = (e) => {
-        const imageData = e.target.result;
-        setImages({ primary: imageData });
-        sendImage({ primary: imageData });
-      };
-
-      reader.readAsDataURL(file);
-    }
-  };
-
-  const handleFileClick = (event) => {
-    event.target.value = null; // Limpa o valor do input para permitir seleções consecutivas do mesmo arquivo
-  };
 
   const initialValues = {
     image_1: '',
     image_2: '',
     image_3: '',
-    image_4: ''
-  }
+    image_4: '',
+  };
 
-  const onSubmit = values => {
-    console.log(values)
-  }
+  const onSubmit = async (values) => {
+    await sendImage(values);
+    console.log(values);
+  };
 
-  const validate = values => {
-
-  }
+  const validate = (values) => {};
 
   const formik = useFormik({
     initialValues,
     onSubmit,
-    validate
+    validate,
   });
+
+  useEffect(() => {
+    if (submit) {
+      formik.handleSubmit();
+    }
+  }, [submit]);
+
+  const handleImageChange = (fieldName, file) => {
+    formik.setFieldValue(fieldName, file);
+    setImages((prevImages) => ({
+      ...prevImages,
+      [fieldName]: URL.createObjectURL(file),
+    }));
+  };
+
+  const renderImagePreview = (fieldName) => {
+    const imagePreview = images[fieldName];
+    if (imagePreview) {
+      return <img src={imagePreview} alt="Preview" />;
+    }
+    return null;
+  };
 
   return (
     <section className="container-upload-image">
       <h2>Adicione imagens do produto</h2>
-      
-      <form onSubmit={formik.handleSubmit} className="images-uploaded">
 
+      <form className="images-uploaded">
         <label htmlFor="image_1" className="primary-input-label">
-
           <div className="container-icon-image-upload">
-            <FontAwesomeIcon 
-              className="icon-image-upload" 
-              icon={faImages} />
-
+            <FontAwesomeIcon className="icon-image-upload" icon={faImages} />
           </div>
 
           <span className="text-icon-image-upload">Escolha uma imagem</span>
@@ -89,52 +66,50 @@ export default function UploadImage({ submit }) {
           <input
             type="file"
             name="image_1"
-            {...formik.getFieldProps("image_1")}
+            onChange={(event) =>
+              handleImageChange('image_1', event.target.files[0])
+            }
           />
-            
-
+          {renderImagePreview('image_1')}
         </label>
 
         <label htmlFor="image_2" className="secondary-input-label">
-
-          <FontAwesomeIcon 
-            className="icon-image-plus" 
-            icon={faPlus} />
+          <FontAwesomeIcon className="icon-image-plus" icon={faPlus} />
 
           <input
             type="file"
             name="image_2"
-            {...formik.getFieldProps("image_2")}
+            onChange={(event) =>
+              handleImageChange('image_2', event.target.files[0])
+            }
           />
-
+          {renderImagePreview('image_2')}
         </label>
 
         <label htmlFor="image_3" className="secondary-input-label">
-
-          <FontAwesomeIcon 
-            className="icon-image-plus" 
-            icon={faPlus} />
+          <FontAwesomeIcon className="icon-image-plus" icon={faPlus} />
 
           <input
             type="file"
             name="image_3"
-            {...formik.getFieldProps("image_3")}
+            onChange={(event) =>
+              handleImageChange('image_3', event.target.files[0])
+            }
           />
-    
+          {renderImagePreview('image_3')}
         </label>
 
         <label htmlFor="image_4" className="secondary-input-label">
-          
-          <FontAwesomeIcon 
-            className="icon-image-plus" 
-            icon={faPlus} />
-          
+          <FontAwesomeIcon className="icon-image-plus" icon={faPlus} />
+
           <input
             type="file"
             name="image_4"
-            {...formik.getFieldProps("image_4")}
+            onChange={(event) =>
+              handleImageChange('image_4', event.target.files[0])
+            }
           />
-          
+          {renderImagePreview('image_4')}
         </label>
       </form>
     </section>
@@ -142,5 +117,6 @@ export default function UploadImage({ submit }) {
 }
 
 UploadImage.propTypes = {
-  submit: PropTypes.func.isRequired,
+  submit: PropTypes.bool.isRequired,
+  sendImage: PropTypes.func.isRequired,
 };
